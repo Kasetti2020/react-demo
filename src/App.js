81ea97269628819dc5f8b73f7a6c80d9726f6dd1
sharan018 <115627304+sharan018@users.noreply.github.com>
2023-07-07 11:29:29 +0530

    react

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>hello</h1>
+       <h1>hello world</h1>
        <Components/>
       </header>
     </div>
